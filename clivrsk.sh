8a7e99bae8478bfd1d9228c6678c9e62f815d18e
chudinov1987 <111620860+chudinov1987@users.noreply.github.com>
2022-08-20 19:56:44 +0800

    Update clivrsk.sh

--- clivrsk.sh
+++ clivrsk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -P /home/ubuntu/ https://raw.githubusercontent.com/chudinov1987/Testing/main/vrsk.sh
+wget -P /home/ubuntu https://raw.githubusercontent.com/chudinov1987/Testing/main/vrsk.sh
 chmod +x /home/ubuntu/script.sh
 sleep 5
 sudo /home/ubuntu/script.sh

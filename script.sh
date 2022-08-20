5a4d6e48a777f680f8e808a9e5718d685abf23e1
chudinov1987 <111620860+chudinov1987@users.noreply.github.com>
2022-08-20 11:10:21 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 echo -e 'deb http://ftp.de.debian.org/debian stretch main \ndeb http://security.debian.org/debian-security stretch/updates main' >> /etc/apt/sources.list
-apt update && apt full-upgrade -y && apt install net-tools ethtool linux-igd nload mc htop docker.io -y
+apt update 
+apt full-upgrade -y
+apt install net-tools ethtool linux-igd nload mc htop docker.io -y
 echo -e 'EXTIFACE=eth0\nINTIFACE=docker0' >> /etc/default/linux-igd
 echo -e 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
 /etc/init.d/linux-igd restart && sysctl -p 
+cd /root
 touch Dockerfile
 echo -e 'FROM debian:buster-slim' >> Dockerfile
 echo -e 'RUN cd /tmp && apt update && apt full-upgrade -y && apt install wget libglib2.0-0 netbase -y &&\ ' >> Dockerfile

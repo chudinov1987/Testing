ce2da6bde5cd4504c460dc463fbe85f5c1aa2e24
chudinov1987 <111620860+chudinov1987@users.noreply.github.com>
2022-08-20 11:14:58 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+wget -P /root https://raw.githubusercontent.com/chudinov1987/Testing/main/script.sh 
 echo -e 'deb http://ftp.de.debian.org/debian stretch main \ndeb http://security.debian.org/debian-security stretch/updates main' >> /etc/apt/sources.list
 apt update 
 apt full-upgrade -y

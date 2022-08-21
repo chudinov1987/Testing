af35b824ba283f09a293b1514ecca90df7667d01
chudinov1987 <111620860+chudinov1987@users.noreply.github.com>
2022-08-21 12:01:11 +0700

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 wget -P /home/ubuntu https://raw.githubusercontent.com/chudinov1987/Testing/main/script.sh 
 chmod +x /home/ubuntu/script.sh
-sleep 5
+sleep 6
 sudo /home/ubuntu/script.sh

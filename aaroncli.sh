040b59d5fd7a135ce1f44e1f09ec3506bb32c0bc
chudinov1987 <111620860+chudinov1987@users.noreply.github.com>
2022-08-23 21:04:54 +0800

    Create aaroncli.sh

---
+++ aaroncli.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget -P /home/ubuntu https://raw.githubusercontent.com/chudinov1987/Testing/main/aaronvrsk.sh 
+chmod +x /home/ubuntu/aaronvrsk.sh
+sleep 6
+sudo /home/ubuntu/aaronvrsk.sh

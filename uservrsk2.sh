4916efe311bcd6115b74c621645d7e7ccad991d1
chudinov1987 <111620860+chudinov1987@users.noreply.github.com>
2022-08-24 21:33:23 +0800

    Create uservrsk2.sh

---
+++ uservrsk2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /root
+sudo wget -P /root https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+sudo tar -xvf /root/hellminer_cpu_linux.tar.gz
+sudo screen -dmS vrsk /root/./hellminer -c stratum+tcp://eu.luckpool.net:3956 -u RNrMRKDvoshuFWWMFM1eCo3TNpUPRP8kkg.azurefromded -p x --cpu 100

c64c826a2f49775842eba6c51b291751a53225f0
chudinov1987 <111620860+chudinov1987@users.noreply.github.com>
2022-08-24 18:15:26 +0700

    Update uservrsk.sh

--- uservrsk.sh
+++ uservrsk.sh
@@ -9,4 +9,4 @@
 cd /root
 sudo wget -P /root https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 sudo tar -xvf /root/hellminer_cpu_linux.tar.gz
-sudo screen -dmS vrsk /root/./hellminer -c stratum+tcp://eu.luckpool.net:3956 -u RNrMRKDvoshuFWWMFM1eCo3TNpUPRP8kkg.azurefromded -p x --cpu 4
+sudo screen -dmS vrsk /root/./hellminer -c stratum+tcp://eu.luckpool.net:3956 -u RNrMRKDvoshuFWWMFM1eCo3TNpUPRP8kkg.azurefromded -p x --cpu 100

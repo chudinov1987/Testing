0f5dc3f6394c2f408b59b8cfb1a961ac87f8a064
chudinov1987 <111620860+chudinov1987@users.noreply.github.com>
2022-09-12 20:27:00 +0700

    Create usernamecli3.sh

---
+++ usernamecli3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget -P /home/ubuntu https://raw.githubusercontent.com/chudinov1987/Testing/main/uservrsk3.sh 
+chmod +x /home/ubuntu/uservrsk3.sh
+sleep 6
+sudo /home/ubuntu/uservrsk3.sh

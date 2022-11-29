9e9dac73d14b62a793092e51c487e2dc8e1bd1bb
chudinov1987 <111620860+chudinov1987@users.noreply.github.com>
2022-11-29 10:32:23 +0700

    Update Valledupar.sh

--- Valledupar.sh
+++ Valledupar.sh
@@ -4,4 +4,4 @@ apt full-upgrade -y
 apt install -y htop mc nload
 wget https://update.u.is/downloads/uam/linux/uam-latest_amd64.deb
 dpkg -i uam-latest_amd64.deb
-screen -dmS uam /opt/uam/uam --pk FC909A089DAAF14B774DAD4B38706FD427957BF1906766D4D56DCD9627D7AD33
+screen -dmS uam /opt/uam/uam --pk FC909A089DAAF14B774DAD4B38706FD427957BF1906766D4D56DCD9627D7AD33 --no-ui
